648f381e3d0c0bfde3b740734e1b05f3c02fa8de
Zobam <nnojemabel@gmail.com>
2022-09-14 17:53:18 -0700

    omo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int_putchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,7 +14,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
 
 #endif
 
